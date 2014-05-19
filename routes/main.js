77b06eedf37b80a596a1d1db6dee8b6f3652d799
cpancake <anrogers529@gmail.com>
2014-05-19 03:34:09 -0400

    Nevermind, I guess

--- routes/main.js
+++ routes/main.js
@@ -9,7 +9,7 @@ exports.initialize = function(_Site)
 	nconf = Site.Config;
 
 	app.get('/', Site.Util.ensureAuthenticated, function(req, res) {
-		res.render('index', { domain: (process.env.NODE_ENV == 'production' ? Site.Domain + ':' + Site.Port : Site.Domain), auth: req.user.sessionID });
+		res.render('index', { domain: Site.Domain, auth: req.user.sessionID });
 	});
 
 	if(process.env.NODE_ENV != 'production')
